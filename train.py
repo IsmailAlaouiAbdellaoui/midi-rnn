3528317d54b477a6ef2c0ca23f0880011bf6441b
Ismail Alaoui Abdellaoui <i.alaouiabdellaoui@student.maastrichtuniversity.nl>
2019-11-09 19:58:13 +0100

    minor bug fix from fork

--- train.py
+++ train.py
@@ -166,7 +166,7 @@ def main():
                       for path in os.listdir(args.data_dir) \
                       if '.mid' in path or '.midi' in path]
     except OSError as e:
-        log('Error: Invalid --data_dir, {} directory does not exist. Exiting.', args.verbose)
+        utils.log('Error: Invalid --data_dir, {} directory does not exist. Exiting.', args.verbose)
         exit(1)
         
     print(args.data_dir)
